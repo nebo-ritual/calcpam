722b70b8691ceea7d9f825049fdc2d3631b97070
nebo-ritual <56114191+nebo-ritual@users.noreply.github.com>
2019-10-03 13:42:44 +0300

    Add files via upload

---
+++ precache-manifest.b4c695bb8e76f5bc1bcdf3d49de24b34.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41048d6dc827c716ec3fb9ff34d768c2",
+    "url": "/calcpam/index.html"
+  },
+  {
+    "revision": "47165b58675d247f3e7b",
+    "url": "/calcpam/static/css/main.e9411993.chunk.css"
+  },
+  {
+    "revision": "3d622439a0ad17f97618",
+    "url": "/calcpam/static/js/2.daa5dd65.chunk.js"
+  },
+  {
+    "revision": "47165b58675d247f3e7b",
+    "url": "/calcpam/static/js/main.4aaffd35.chunk.js"
+  },
+  {
+    "revision": "10999e5106a1fd06e4e6",
+    "url": "/calcpam/static/js/runtime~main.3cb9319e.js"
+  },
+  {
+    "revision": "76306f46fb4638069ae7a33975ffef8a",
+    "url": "/calcpam/static/media/c01.76306f46.png"
+  },
+  {
+    "revision": "92c6523e25d63dd05a5a3eab1df22f3d",
+    "url": "/calcpam/static/media/c01.92c6523e.jpg"
+  },
+  {
+    "revision": "166c9c9b2bea53a7b1f10d66c68f3348",
+    "url": "/calcpam/static/media/c02.166c9c9b.jpg"
+  },
+  {
+    "revision": "8785e80a7f2d524ba86f2b4262ca8b0d",
+    "url": "/calcpam/static/media/c02.8785e80a.png"
+  },
+  {
+    "revision": "178115b2d426512d8676bdff6f3fc2d6",
+    "url": "/calcpam/static/media/c03.178115b2.jpg"
+  },
+  {
+    "revision": "6a938ee85d62c10bf0f26a152e3c05e6",
+    "url": "/calcpam/static/media/c03.6a938ee8.png"
+  },
+  {
+    "revision": "85e859c95afa1af7cd0956b1fd81f91d",
+    "url": "/calcpam/static/media/c04.85e859c9.jpg"
+  },
+  {
+    "revision": "f5650b868a22032f1b1f30d5b1cd7ef7",
+    "url": "/calcpam/static/media/c04.f5650b86.png"
+  },
+  {
+    "revision": "5cb0f581e150ede845f7ecde1c95a6e9",
+    "url": "/calcpam/static/media/f01.5cb0f581.png"
+  },
+  {
+    "revision": "ee6297e163c051566eaecd3dd4272146",
+    "url": "/calcpam/static/media/f02.ee6297e1.png"
+  },
+  {
+    "revision": "5b6042f3646b7d099d6aec80eb39d9fb",
+    "url": "/calcpam/static/media/fn01.5b6042f3.png"
+  },
+  {
+    "revision": "077737c240d66f5bdeb47c8f0822fc7f",
+    "url": "/calcpam/static/media/p01.077737c2.png"
+  },
+  {
+    "revision": "5b217cacc31a553eccc88b3164f3354b",
+    "url": "/calcpam/static/media/p02.5b217cac.png"
+  },
+  {
+    "revision": "b3af59f29acf9fb6384a0fc83738960e",
+    "url": "/calcpam/static/media/p03.b3af59f2.png"
+  },
+  {
+    "revision": "b8abd2ef912899a084bc700d13dd28f6",
+    "url": "/calcpam/static/media/s01.b8abd2ef.png"
+  },
+  {
+    "revision": "c2aaeab6d36b2d63c21c2dd2293f533b",
+    "url": "/calcpam/static/media/s01pt01.c2aaeab6.png"
+  },
+  {
+    "revision": "004bddc397531b6dc7e766f5d2046539",
+    "url": "/calcpam/static/media/s01pt02.004bddc3.png"
+  },
+  {
+    "revision": "ff6f98ef3268371cc11b53ff1ca72f6c",
+    "url": "/calcpam/static/media/s03.ff6f98ef.png"
+  },
+  {
+    "revision": "0bf1fe4832a68efcf57f4a3e727b3e71",
+    "url": "/calcpam/static/media/s03pt01.0bf1fe48.png"
+  },
+  {
+    "revision": "1075600e73003251fed4eb96da859f36",
+    "url": "/calcpam/static/media/s03pt02.1075600e.png"
+  },
+  {
+    "revision": "4580bee5f210f82db1dea061a231a33a",
+    "url": "/calcpam/static/media/s04.4580bee5.png"
+  },
+  {
+    "revision": "e6f0654c57cbdb99ee8629b26cc93999",
+    "url": "/calcpam/static/media/s04pt01.e6f0654c.png"
+  },
+  {
+    "revision": "ac226157d6068d538ee8983ece9eebb7",
+    "url": "/calcpam/static/media/s04pt02.ac226157.png"
+  },
+  {
+    "revision": "3da600c8250d0fa6110a37d89b14f7b9",
+    "url": "/calcpam/static/media/s07.3da600c8.png"
+  },
+  {
+    "revision": "5d8a29508f8acfc333da8374f4919e27",
+    "url": "/calcpam/static/media/s07pt02.5d8a2950.png"
+  },
+  {
+    "revision": "dbaeb55914dcb27dbffedc118271a653",
+    "url": "/calcpam/static/media/s08.dbaeb559.png"
+  },
+  {
+    "revision": "96a7f149cc087fb2712fa7a1d792b06e",
+    "url": "/calcpam/static/media/s08pt01.96a7f149.png"
+  },
+  {
+    "revision": "3e493f6afe188f9609de3bdf8d6e573a",
+    "url": "/calcpam/static/media/s08pt02.3e493f6a.png"
+  },
+  {
+    "revision": "5b9dafa156e625214ca4df3fc9ade9c5",
+    "url": "/calcpam/static/media/s09.5b9dafa1.png"
+  },
+  {
+    "revision": "7512f41284b3652b27ed09fc55734e7b",
+    "url": "/calcpam/static/media/s09pt01.7512f412.png"
+  },
+  {
+    "revision": "76147e11e6c37f4497f47a8b365124d2",
+    "url": "/calcpam/static/media/s09pt02.76147e11.png"
+  },
+  {
+    "revision": "4ec1cfa3587882eb88ad182ee3205711",
+    "url": "/calcpam/static/media/s10.4ec1cfa3.png"
+  },
+  {
+    "revision": "de03a8262af5a5fffb90ea8be4d88e07",
+    "url": "/calcpam/static/media/s10pt01.de03a826.png"
+  },
+  {
+    "revision": "9d694ddb2fb1dd4cbbffe1b4e60eaff8",
+    "url": "/calcpam/static/media/s10pt02.9d694ddb.png"
+  },
+  {
+    "revision": "37533fac69da418c4deee089df91d7ff",
+    "url": "/calcpam/static/media/s13.37533fac.png"
+  },
+  {
+    "revision": "915858ea3d3a24046318c7a9d056a321",
+    "url": "/calcpam/static/media/s13pt01.915858ea.png"
+  },
+  {
+    "revision": "b5309d99900848665209f0f92c1059c5",
+    "url": "/calcpam/static/media/s13pt02.b5309d99.png"
+  },
+  {
+    "revision": "674791a08b16f0f0019e6a907cae0757",
+    "url": "/calcpam/static/media/s16.674791a0.png"
+  },
+  {
+    "revision": "e0d6f706a3f1b59a210cd2f1209496fc",
+    "url": "/calcpam/static/media/s16pt01.e0d6f706.png"
+  },
+  {
+    "revision": "932f778c6a4a85e4d981b80a4e55ba42",
+    "url": "/calcpam/static/media/s16pt02.932f778c.png"
+  },
+  {
+    "revision": "e099cd96b62c39d5a4082702a0e904a6",
+    "url": "/calcpam/static/media/s17.e099cd96.png"
+  },
+  {
+    "revision": "8ca20ebd007ed6d40b20bdcc4f00eda6",
+    "url": "/calcpam/static/media/s17pt01.8ca20ebd.png"
+  },
+  {
+    "revision": "89a0e144db37af5c503b4ee7a1fa9e35",
+    "url": "/calcpam/static/media/s17pt02.89a0e144.png"
+  },
+  {
+    "revision": "a13598a5c86005df866e2e097f94d974",
+    "url": "/calcpam/static/media/s18.a13598a5.png"
+  },
+  {
+    "revision": "ab1900f1c8caa3ec8f329e494f3ee300",
+    "url": "/calcpam/static/media/s18pt01.ab1900f1.png"
+  },
+  {
+    "revision": "efdb7a972345389d8cd42fc3653576f0",
+    "url": "/calcpam/static/media/s18pt02.efdb7a97.png"
+  },
+  {
+    "revision": "66cf95ac7ef7626e08decec6d967f5ba",
+    "url": "/calcpam/static/media/s19.66cf95ac.png"
+  },
+  {
+    "revision": "e92872d8b7352e33ab15162c3afe22bb",
+    "url": "/calcpam/static/media/s19pt01.e92872d8.png"
+  },
+  {
+    "revision": "dc1e5204feddb6814c21634ee7b56a53",
+    "url": "/calcpam/static/media/s19pt02.dc1e5204.png"
+  },
+  {
+    "revision": "806d622a48a71f435dee6acd9e5616be",
+    "url": "/calcpam/static/media/v02.806d622a.png"
+  }
+]);
